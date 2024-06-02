b3b81f95e49f43b80d2abe41637e95d520492f91
Dinh Trung <trantrung140219@gmail.com>
2024-06-01 22:13:53 -0500

    add Lab06 - ASCII Animation Viewer

---
+++ w1d6/ascii.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+    "use strict";
+
+    const textArea = document.getElementById("text-area");
+    const startBtn = document.getElementById("start");
+    const stopBtn = document.getElementById("stop");
+    const animationCustom = document.getElementById("animation");
+    const fontSizeCustom = document.getElementById("fontsize");
+    const turboCheckbox = document.getElementById("turbo");
+
+    let animationInterval = null;
+    let frameIndex = 0;
+    let frames = [];
+    const defaultInterval = 250;
+    const turboInterval = 50;
+
+    function setAnimationInterval() {
+        const interval = turboCheckbox.checked ? turboInterval : defaultInterval;
+        animationInterval = setInterval(() => {
+            textArea.value = frames[frameIndex++ % frames.length];
+        }, interval);
+    }
+
+    function startAnimation() {
+        frames = textArea.value.split("=====\n");
+        frameIndex = 0;
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+        animationCustom.disabled = true;
+
+        setAnimationInterval();
+    }
+
+    function stopAnimation() {
+        clearInterval(animationInterval);
+        animationInterval = null;
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+        animationCustom.disabled = false;
+        textArea.value = frames.join("=====\n");
+    }
+
+    startBtn.addEventListener("click", startAnimation);
+    stopBtn.addEventListener("click", stopAnimation);
+
+    animationCustom.addEventListener("change", function() {
+        const selectedAnimation = animationCustom.value;
+        textArea.value = ANIMATIONS[selectedAnimation] || "";
+    });
+
+    fontSizeCustom.addEventListener("change", function() {
+        textArea.style.fontSize = fontSizeCustom.value;
+    });
+
+    turboCheckbox.addEventListener("change", function() {
+        if (animationInterval) {
+            clearInterval(animationInterval);
+            setAnimationInterval();
+        }
+    })
+}
